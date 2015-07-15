f5bfd94cc0f35dd9b03f383354110b4427b5b90e
iagows <el.iagows@gmail.com>
2015-07-15 11:40:01 -0300

    Ações dos botões.

--- PetriWidget/petridesign.cpp
+++ PetriWidget/petridesign.cpp
@@ -6,9 +6,89 @@ PetriDesign::PetriDesign(QWidget *parent) :
     ui(new Ui::PetriDesign)
 {
     ui->setupUi(this);
+
+    this->bGroup = new QButtonGroup();
+    this->addButtonsToGroup();
+
+    connect(this->ui->widget, SIGNAL(sceneClicked()),
+            this, SLOT(afterClickAction()));
 }
 
 PetriDesign::~PetriDesign()
 {
     delete ui;
+    delete bGroup;
+}
+
+void PetriDesign::on_bt_normal_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::MoveItem);
+}
+
+void PetriDesign::on_bt_place_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsItem);
+    this->ui->widget->getScene()->setItemType(IPetriItem::Place);
+}
+
+void PetriDesign::on_bt_fplace_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsItem);
+    this->ui->widget->getScene()->setItemType(IPetriItem::FPlace);
+}
+
+void PetriDesign::on_bt_ttrans_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsItem);
+    this->ui->widget->getScene()->setItemType(IPetriItem::TTrans);
+}
+
+void PetriDesign::on_bt_itrans_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsItem);
+    this->ui->widget->getScene()->setItemType(IPetriItem::ITrans);
+}
+
+void PetriDesign::on_bt_remove_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::RemoveItem);
+}
+
+void PetriDesign::on_bt_arc_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsArc);
+    this->ui->widget->getScene()->setArcType(IPetriArc::Activator);
+}
+
+void PetriDesign::on_bt_iarc_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsArc);
+    this->ui->widget->getScene()->setArcType(IPetriArc::Inhibitor);
+}
+
+void PetriDesign::on_bt_farc_clicked()
+{
+    this->ui->widget->getScene()->setMode(PetriScene::InsArc);
+    this->ui->widget->getScene()->setArcType(IPetriArc::FActivator);
+}
+
+void PetriDesign::afterClickAction()
+{
+    if(!this->ui->cb_lock->isChecked())
+    {
+        this->ui->bt_normal->click();
+    }
+}
+
+void PetriDesign::addButtonsToGroup()
+{
+    this->bGroup->addButton(this->ui->bt_arc);
+    this->bGroup->addButton(this->ui->bt_farc);
+    this->bGroup->addButton(this->ui->bt_fplace);
+    this->bGroup->addButton(this->ui->bt_iarc);
+    this->bGroup->addButton(this->ui->bt_itrans);
+    this->bGroup->addButton(this->ui->bt_normal);
+    this->bGroup->addButton(this->ui->bt_place);
+    this->bGroup->addButton(this->ui->bt_remove);
+    this->bGroup->addButton(this->ui->bt_ttrans);
 }
