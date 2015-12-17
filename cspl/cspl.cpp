2a1691c21312b95a012e7beadb99a336e247bf7d
Iago Alves da Silva <el.iagows@gmail.com>
2015-12-16 21:44:59 -0300

    CSPL
    Adição das funções mínimas e necessárias.

--- cspl/cspl.cpp
+++ cspl/cspl.cpp
@@ -8,18 +8,108 @@ Cspl::Cspl()
 std::string Cspl::to_ascii_c(spnp::Net *input)
 {
     this->net = input;
-    std::string content("");
 
-    content = addComment();
+    addComment();
+    ss<<"\n";
+    addIncludes();
+    ss<<"\n";
+    addGlobalVars();
+    ss<<"\n";
+    addOptions();
+    ss<<"\n";
+    addNet();
+    ss<<"\n";
+    addAssert();
+    ss<<"\n";
+    addAc_Init();
+    ss<<"\n";
+    addAc_Reach();
+    ss<<"\n";
+    addAc_Final();
+    ss<<"\n";
 
-    return content;
+    return ss.str();
 }
 
-std::string Cspl::addComment()
+void Cspl::addComment()
 {
-    return "\/*\n*\tCriado pela ferramenta SPNP new GUI.\n"
-            "*\tDesenvolvida por Iago Alves da Silva como um substituto\n"
-            "*\tdo SPNP-Gui [por K. S. Trivedi - Duke]\n"
-            "*\n"
-            "*/";
+    this->ss << "\/*\n*\tCriado pela ferramenta SPNP new GUI.\n"
+             << "*\tDesenvolvida por Iago Alves da Silva como um substituto\n"
+             << "*\tdo SPNP-Gui [por K. S. Trivedi - Duke]\n"
+             << "*/\n";
+}
+
+void Cspl::addIncludes()
+{
+    std::vector<std::string> *v = new std::vector<std::string>();
+    v->push_back("user.h");
+
+    //TODO adicionar de acordo com o projeto
+
+    for (int i = 0, total = v->size(); i < total; ++i)
+    {
+        ss << "#include " << v->at(i) << "\n";
+    }
+    delete v;
+}
+
+void Cspl::addGlobalVars()
+{
+
+}
+
+void Cspl::addOptions()
+{
+    ss << "void options() {\n";
+
+    //TODO adicionar opções do projeto
+
+    ss << "}\n";
+}
+
+void Cspl::addNet()
+{
+    ss << "void net() {\n";
+
+    //TODO adicionar rede
+
+    ss << "}\n";
+}
+
+void Cspl::addAssert()
+{
+    ss << "int assert() {\n";
+
+    //TODO adicionar red
+
+    ss<< "return -1;\n";
+
+    ss << "}\n";
+}
+
+void Cspl::addAc_Init()
+{
+    ss << "void ac_init() {\n";
+
+    //TODO adicionar init
+
+    ss << "}\n";
+}
+
+void Cspl::addAc_Reach()
+{
+    ss << "void ac_reach() {\n";
+
+    //TODO adicionar reach
+
+    ss << "}\n";
+}
+
+void Cspl::addAc_Final()
+{
+    ss << "void ac_final() {\n";
+
+    //TODO adicionar final
+
+    ss << "}\n";
 }
--- cspl/cspl.h
+++ cspl/cspl.h
@@ -3,6 +3,7 @@
 
 #include "cspl_global.h"
 #include "objs/net.h"
+#include <sstream>
 
 class CSPL_PUBLIC Cspl
 {
@@ -11,10 +12,20 @@ public:
     Cspl();
     std::string to_ascii_c(spnp::Net *input);
 
-    std::string addComment();
+    void addComment();
+    void addIncludes();
+    void addGlobalVars();
+    void addOptions();
+    void addNet();
+    void addAssert();
+    void addAc_Init();
+    void addAc_Reach();
+    void addAc_Final();
+
 
 private:
     spnp::Net* net;
+    std::stringstream ss;
 };
 
 #endif // CSPL_H
